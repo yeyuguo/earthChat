27e738ee11f8c49897df2a40044484368c11c6b9
yeyuguo <346243440@qq.com>
2017-02-13 11:25:41 +0800

    添加头像和对话转换

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-;(function(w, d3, undefined){
+;
+(function(w, d3, undefined) {
     "use strict";
 
     var width, height;
-    function getSize(){
-        width = w.innerWidth,
-        height = w.innerHeight;
 
-        if(width === 0 || height === 0){
-            setTimeout(function(){
+    function getSize() {
+        width = w.innerWidth,
+            height = w.innerHeight;
+
+        if (width === 0 || height === 0) {
+            setTimeout(function() {
                 getSize();
             }, 100);
-        }
-        else {
+        } else {
             init();
         }
     }
 
-    function init(){
+    function init() {
 
         //Setup path for outerspace
         var space = d3.geo.azimuthal()
@@ -51,25 +52,25 @@
 
         var svg = d3.select("body")
             .append("svg")
-                .attr("width", width)
-                .attr("height", height)
-                .append("g")
-                    .call(zoom)
-                    .on("dblclick.zoom", null);
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .call(zoom)
+            .on("dblclick.zoom", null);
 
         //Create a list of random stars and add them to outerspace
         var starList = createStars(300);
-                
+
         var stars = svg.append("g")
             .selectAll("g")
             .data(starList)
             .enter()
             .append("path")
-                .attr("class", "star")
-                .attr("d", function(d){
-                    spacePath.pointRadius(d.properties.radius);
-                    return spacePath(d);
-                });
+            .attr("class", "star")
+            .attr("d", function(d) {
+                spacePath.pointRadius(d.properties.radius);
+                return spacePath(d);
+            });
 
 
         svg.append("rect")
@@ -88,6 +89,12 @@
 
         var g = svg.append("g"),
             features;
+        // 标记点
+        var beijingPoint, PopupBox, textBox, picBox
+
+        var dialogBox = svg.append("g")
+            .attr("id", "dialogBox")
+
 
         //Add all of the countries to the globe
         d3.json("world-countries.json", function(collection) {
@@ -95,27 +102,31 @@
 
             features.enter().append("path")
                 .attr("class", "feature")
-                .attr("d", function(d){ return path(circle.clip(d)); });
+                .attr("d", function(d) { return path(circle.clip(d)); });
         });
 
         //Redraw all items with new projections
-        function redraw(){
-            features.attr("d", function(d){
+        function redraw() {
+            markPoint()
+
+            features.attr("d", function(d) {
                 return path(circle.clip(d));
             });
 
-            stars.attr("d", function(d){
+            stars.attr("d", function(d) {
                 spacePath.pointRadius(d.properties.radius);
                 return spacePath(d);
             });
+
+
         }
 
 
         function move() {
-            if(d3.event){
+            if (d3.event) {
                 var scale = d3.event.scale;
                 var origin = [d3.event.translate[0] * -1, d3.event.translate[1]];
-                
+
                 projection.scale(scale);
                 space.scale(scale * 3);
                 backgroundCircle.attr('r', scale);
@@ -123,7 +134,7 @@
 
                 projection.origin(origin);
                 circle.origin(origin);
-                
+
                 //globe and stars spin in the opposite direction because of the projection mode
                 var spaceOrigin = [origin[0] * -1, origin[1] * -1];
                 space.origin(spaceOrigin);
@@ -132,9 +143,9 @@
         }
 
 
-        function createStars(number){
+        function createStars(number) {
             var data = [];
-            for(var i = 0; i < number; i++){
+            for (var i = 0; i < number; i++) {
                 data.push({
                     geometry: {
                         type: 'Point',
@@ -149,9 +160,67 @@
             return data;
         }
 
-        function randomLonLat(){
+        function randomLonLat() {
             return [Math.random() * 360 - 180, Math.random() * 180 - 90];
         }
+
+
+        // 标记点
+        function markPoint(point) {
+            var peking = point || [116.3, 39.9];
+            var textCont = textCont || '这是测试对话'
+            dialogBox = d3.select('#dialogBox')
+            var proPeking = projection(peking)
+            if (beijingPoint) {
+                beijingPoint.remove()
+            }
+
+            if (PopupBox) {
+                PopupBox.remove()
+            }
+            if (textBox) {
+                textBox.remove()
+            }
+            if (picBox) {
+                picBox.remove();
+            }
+
+            beijingPoint = dialogBox.append('circle')
+                .attr("cx", proPeking[0])
+                .attr("cy", proPeking[1])
+                .attr("r", 8)
+                .style("fill", "red");
+
+
+            picBox = dialogBox.append("image")
+                .attr("x", proPeking[0] - 30)
+                .attr("y", proPeking[1] - 30)
+                .attr("width", 35)
+                .attr("height", 35)
+                .attr("xlink:href", "duang/img/user-2.png");
+
+
+            PopupBox = dialogBox.append('rect')
+                .attr('class', 'Popup')
+                .attr('x', proPeking[0] + 1)
+                .attr('y', proPeking[1] + 1)
+                .attr('rx', 5)
+                .attr('ry', 5)
+                .attr('width', 100)
+                .attr('height', 30)
+                .attr('fill', 'white')
+
+            textBox = dialogBox.append('text')
+                .attr('class', 'Popup')
+                .attr('x', proPeking[0] + 5)
+                .attr('y', proPeking[1] + 20)
+                .html(textCont)
+
+            beijingPoint.on("click", function() {
+                console.log(d3.select(this))
+            })
+        }
+        markPoint()
 
     }
 
