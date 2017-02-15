55e48221fe884274746a3146ce7b103d1abff396
yeyuguo <346243440@qq.com>
2017-02-15 10:33:04 +0800

    完成简单的 socket.io Demo 1.完成 socket.io 简单的Demo。

--- websocket/server.js
+++ websocket/server.js
@@ -1,15 +1,31 @@
+var express  = require('express')
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 
 
+app.use(express.static(__dirname + '/'));
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/client.html');
 })
 
-io.on('connection',function(){
-    console.log('new user connected');
-})
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    socket.on('disconnect',function(){
+        console.log('user is logout');
+    })
+
+    socket.on('client send',function(msg){
+        console.log('receive message is :',msg);
+        socket.emit('server send',msg)
+    })
+
+
+    
+    
+
+});
 
 http.listen(3001,function(){
     console.log('listening 3001 port');
