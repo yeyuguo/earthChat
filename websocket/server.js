6b467f204ed183427be594c4cf17d1328ec1a2c8
yeyuguo <346243440@qq.com>
2017-02-16 14:54:23 +0800

    socket 对话基本完成

--- websocket/server.js
+++ websocket/server.js
@@ -8,26 +8,42 @@ app.use(express.static(__dirname + '/'));
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client.html');
 })
-
+var onlineUser = {}
 io.on('connection', function(socket) {
-    var onlineUser = {}
+    // socket.name = null;
     console.log('a user connected');
-
+    // console.log('socket name :',socket.name)
     socket.on('disconnect', function() {
-        console.log('user is logout');
+        if(onlineUser.hasOwnProperty(socket.name)){
+            delete onlineUser[socket.name]
+        }
+        // console.log('user is logout');
+        console.log('disconnected obj:',socket.name)
+        io.emit('disconnect', onlineUser)
     })
 
     socket.on('client send', function(clientObj) {
-        console.log('receive message is :', clientObj);
+        
+        // console.log('receive message is :', clientObj);
         if (!clientObj.userid) {
             return false
         }
+        // console.log('socket name :',socket.name)
+        // 首次进入 记录唯一 ID,便于 onlineUser的变化
+        if(!socket.name){
+            socket.name = clientObj.userid;
+        }
+        // 设置 onlineUser
         var userid = clientObj.userid
         if (!onlineUser.hasOwnProperty(userid)) {
             onlineUser[userid] = clientObj.username
         }
-        clientObj.onlineUser = onlineUser
-        socket.emit('server send', clientObj)
+        // 判断 onlineUser 是否有变化
+        if(clientObj.onlineUser != onlineUser){
+            clientObj.onlineUser = onlineUser
+        }
+        // 此处不能用 socket.emit 来返回，否则的话就被锁定在一个socket里
+        io.emit('server send', clientObj)
     })
 });
 
